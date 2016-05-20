95de68dd21b90982fa2128a8f7173642ae4ba984
Phil <Phil@Phil-PC.lan>
2016-05-20 19:01:36 +0100

    updated to version 2.

--- server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-console.log(“server running...”);
-http.createServer(function (req, res) {
-console.log('request received');
-res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('_testcb(\'{"message": "Hello world!"}\')');
-}).listen(80);
